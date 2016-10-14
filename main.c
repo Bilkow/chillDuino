d5a768ca9914abfd5697b3e2cc71343b16ec4662
Bilko <Bilko@Burga.local>
2016-10-14 03:30:45 -0300

    PWM (seems) to be working! (with timer2)

--- main.c
+++ main.c
@@ -13,18 +13,15 @@
 #define TEMPO_AMOSTRAGEM 30000 // 0.120/0.000004
 int desvio_acumulado;
 
-// unsigned long int tempo_inicial;
-// unsigned long int delta_t[5];
 unsigned char numero_interrupt;
-// unsigned long int milis;
 unsigned char PWM_val;
 
 int main(void) {
     desvio_acumulado = 0;
     numero_interrupt = 0;
-    PWM_val = 255;
+    PWM_val = 127;
 
-    DDRB = 0b10010000;                  // initialize port with bit7 and bit4 as output
+    DDRB |= (1 << 7);                  // initialize port with bit7 as output (led)
     PORTE |= 1 << 4; // seta o pullup do pino 2;
 
     cli(); // clear interruptions
@@ -33,16 +30,12 @@ int main(void) {
 
     // milis = 0;
 
-    // set compare match register to desired timer count:
-    // OCR1A = 1599; // interrompe a cada 0.1 ms
-    // turn on CTC modPORTBe:
-    // TCCR1B |= (1 << WGM12);
-    // Set CS10 and CS12 bits for 1024 prescaler:
-    // TCCR1B |= (1 << CS10);
-    // TCCR1B |= (1 << CS12);
-    // enable timer compare interrupt:
-    // TIMSK1 |= (1 << OCIE1A);
+    // config do fast pwm!!
+    DDRB |= (1 << 4);
+    TCCR2A |= (1 << COM2A1) + (1 << WGM21) + (1 << WGM20); // tabelas 20-3, 20-8
 
+    TCCR2B |= (1 << CS20); // seção 20.10.2
+    OCR2A = 127; // seção 20.10.4
 
     // new clock: no interruptions!!
     // select clock with prescaler = 64 (12 bits de resolucao)!!! (tabela 17-6)
@@ -63,7 +56,7 @@ int main(void) {
     while(1) {
         // uartSendString("spam", 4);
         if (UCSR0A & (1 << RXC0)) {
-            PWM_val = UDR0;
+            OCR2A = UDR0;
             // char recv_byte = UDR0, time_read[2];
             // time_read[0] = TCNT1L;
             // time_read[1] = TCNT1H;
@@ -75,17 +68,11 @@ int main(void) {
 
 
 void envia_valor(int valor) {
-    char tamanho, texto_valor[5];
+    char tamanho, texto_valor[10];
     tamanho = sprintf(texto_valor, "%u ", valor);
     uartSendString(texto_valor, tamanho);
 }
 
-//  --- calculo de frequência ---
-// unsigned long int f_rpm;
-// f_rpm = (long int)600000*5/(delta_t[0] + delta_t[1] + delta_t[2] + delta_t[3] + delta_t[4]);
-//
-// sprintf(mensagem, "%8lu", f_rpm);
-// uartSendString(mensagem, 8);
 
 
 // interrupt ventilador
@@ -101,20 +88,15 @@ ISR(INT4_vect) {
         TCNT1H = 0;
         TCNT1L = 0;
         f_rpm = 15000000/periodo; // 60/(tempo/250000)
-        
+
         desvio_acumulado += TEMPO_AMOSTRAGEM - periodo;
-        
-#ifdef DEBUG 
-        char tamanho, texto_valor[5];
-        tamanho = sprintf(texto_valor, " desvio(%d) periodo(%d) ", desvio_acumulado, periodo);
-        uartSendString(texto_valor, tamanho);
-#endif
-        
+
+
         while (desvio_acumulado < 0) { // caso o periodo seja maior que o tempo de amostragem!
             envia_valor(f_rpm);
             desvio_acumulado += TEMPO_AMOSTRAGEM;
         }
-        
+
         if (desvio_acumulado < TEMPO_AMOSTRAGEM)
             envia_valor(f_rpm);
         else
